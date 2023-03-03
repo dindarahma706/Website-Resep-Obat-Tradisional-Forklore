61d33ecf12204dcc934534b3536dae9d795dda6b
Hidayatul Hayat <87045924+Hdytalhayat@users.noreply.github.com>
2023-03-03 20:49:51 +0700

    Add files via upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('role_as')->default('1');
+            $table->tinyInteger('role_as')->default('0');
+            $table->tinyInteger('status')->default('0');
             $table->rememberToken();
             $table->timestamps();
         });
