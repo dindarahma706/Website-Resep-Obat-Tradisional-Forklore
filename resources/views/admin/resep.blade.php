49539d54932951cd1ccdb2c85bbdd5878cfdaafa
aqnada <118709897+aqnada@users.noreply.github.com>
2023-03-05 13:59:23 +0700

    button

--- resources/views/admin/resep.blade.php
+++ resources/views/admin/resep.blade.php
@@ -10,7 +10,11 @@
         <div class="col-md-12">
             <div class="card shadow">
                 <div class="card-header">
-                    <h1>Resep Obat</h1>
+                    <h1>Resep Obat
+                        <a class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button type="button" class="btn btn-primary"href="{{ url('insert-resep') }}">Tambah</button>
+                        </a>
+                    </h1>
                 </div>
                 <div class="card-body">
                     <table class="table">
@@ -35,8 +39,8 @@
                                     <img class="img" src="{{ asset('assets/uploads/resep/'.$item->Image) }}" alt="image here..">    
                                 </td>
                                 <td>
-                                    <a href="{{ url('edit-resep/'.$item->id) }}" class="btn btn-primary">Edit</a>
-                                    <a href="{{ url('delete-resep/'.$item->id) }}" class="btn btn-danger">Delete</a>
+                                    <a href="{{ url('edit-resep/'.$item->id) }}" class="btn btn-outline-primary">Edit</a>
+                                    <a href="{{ url('delete-resep/'.$item->id) }}" class="btn btn-outline-danger">Delete</a>
                                 </td>
                             </tr>
                             @endforeach
