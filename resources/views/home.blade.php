812fb5b81def5449eb22ce417365ff5a95d99edb
DewiAprilia <dewi.aprilia.2105356@students.um.ac.id>
2023-03-05 21:10:19 +0700

    button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,19 @@
 @endsection
 
 @section('content')
+<style>
+  .button {
+    padding: 30px;
+    display: inline-block;
+    margin: 100px 60px 80px 100px;
+    font-size: 40px;
+  }
+  </style>
   <div class="d-flex h-100 text-center" >
     <main class="cover-container d-flex p-3 mx-auto flex-column">
     <p class="lead pt-3">
-      <a href="/form" class="btn btn-lg btn-secondary fw-bold">Empiris</a>
-      <a href="/home" class="btn btn-lg btn-secondary fw-bold">Scientics</a>
+      <a href="/form" class="button btn btn-lg btn-secondary fw-bold">Empiris</a>
+      <a href="/home" class="button btn btn-lg btn-secondary fw-bold">Scientics</a>
     </p>
     </main>
   </div>
