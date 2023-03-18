76e9e4964b4a1422ec1b6f7bf9e065f2d34a4ba2
levina13 <87984577+levina13@users.noreply.github.com>
2023-03-18 20:18:02 +0700

    add index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')  
+@guest
 <div class="d-flex h-100 text-center text-bg-dark">
   <main class="cover-container d-flex p-3 mx-auto flex-column">
     <h1>Cover your page.</h1>
@@ -14,5 +15,17 @@
     </p>
   </main>
 </div>  
+@endguest
+
+  <div class="d-flex h-100 text-center" >
+    <main class="cover-container d-flex p-3 mx-auto flex-column">
+    <p class="lead pt-3">
+      <a href="/empiris" class="button btn btn-lg btn-secondary fw-bold">Empiris</a>
+      <a href="/home" class="button btn btn-lg btn-secondary fw-bold">Scientics</a>
+    </p>
+    </main>
+  </div>
+
+
 
 @endsection
