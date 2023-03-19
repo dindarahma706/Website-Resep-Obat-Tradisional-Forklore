a524956390bbd60dea42be3af25db069f7b62769
Hidayatul Hayat <87045924+Hdytalhayat@users.noreply.github.com>
2023-03-19 20:35:07 +0700

    Mengganti url
    Sebelumnya, muncul tulisan "input berhasil" diganti dengan redirect ke halaman daftar resep

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -86,7 +86,8 @@ class frontendController extends Controller
         
 
         // ResepNew::create($input);
-        return "berhasil input";
+        return redirect('/daftar-resep');
+
         
     }
     public function Edit($id){
